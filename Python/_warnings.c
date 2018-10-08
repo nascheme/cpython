92584471bbced9020f3b4ab3fe98560b00dd2f22
Neil Schemenauer <nas@arctrix.com>
2018-10-07 22:00:55 -0700

    wip: don't set f_globals if we have f_namespace

--- Python/_warnings.c
+++ Python/_warnings.c
@@ -840,7 +840,12 @@ setup_context(Py_ssize_t stack_level, PyObject **filename, int *lineno,
         *lineno = 1;
     }
     else {
-        globals = f->f_globals;
+        if (f->f_namespace) {
+            globals = PyModule_GetDict(f->f_namespace);
+        }
+        else {
+            globals = f->f_globals;
+        }
         *filename = f->f_code->co_filename;
         Py_INCREF(*filename);
         *lineno = PyFrame_GetLineNumber(f);
