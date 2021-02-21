4c8598fbe59a8874484ab153577e7cc4ca31f687
Neil Schemenauer <nas@arctrix.com>
2021-02-21 12:26:22 -0800

    wip: increase pool and arena sizes by 4x

--- Objects/obmalloc.c
+++ Objects/obmalloc.c
@@ -907,8 +907,8 @@ static int running_on_valgrind = -1;
  * Arenas are allocated with mmap() on systems supporting anonymous memory
  * mappings to reduce heap fragmentation.
  */
-#define ARENA_BITS              18
-#define ARENA_SIZE              (1 << ARENA_BITS)     /* 256 KiB */
+#define ARENA_BITS              20
+#define ARENA_SIZE              (1 << ARENA_BITS)     /* 1 MiB */
 
 #ifdef WITH_MEMORY_LIMITS
 #define MAX_ARENAS              (SMALL_MEMORY_LIMIT / ARENA_SIZE)
@@ -917,8 +917,8 @@ static int running_on_valgrind = -1;
 /*
  * Size of the pools used for small blocks.  Must be a power of 2.
  */
-#define POOL_BITS               12                  /* 4 KiB */
-#define POOL_SIZE               (1 << POOL_BITS)    /* 4 KiB */
+#define POOL_BITS               14                  /* 16 KiB */
+#define POOL_SIZE               (1 << POOL_BITS)
 #define POOL_SIZE_MASK          (POOL_SIZE - 1)
 
 #define MAX_POOLS_IN_ARENA  (ARENA_SIZE / POOL_SIZE)
