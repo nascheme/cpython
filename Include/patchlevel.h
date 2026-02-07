eefa57705b83ad56c358848321a17aadddd7d4d7
Neil Schemenauer <nas@arctrix.com>
2026-02-07 13:24:40 -0800

    Adjust version to pretend this is 3.14.

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -21,13 +21,13 @@
 /* Version parsed out into numeric values */
 /*--start constants--*/
 #define PY_MAJOR_VERSION        3
-#define PY_MINOR_VERSION        15
-#define PY_MICRO_VERSION        0
-#define PY_RELEASE_LEVEL        PY_RELEASE_LEVEL_ALPHA
-#define PY_RELEASE_SERIAL       5
+#define PY_MINOR_VERSION        14
+#define PY_MICRO_VERSION        80
+#define PY_RELEASE_LEVEL        PY_RELEASE_LEVEL_BETA
+#define PY_RELEASE_SERIAL       1
 
 /* Version as a string */
-#define PY_VERSION              "3.15.0a5+"
+#define PY_VERSION              "3.14.80b1"
 /*--end constants--*/
 
 
