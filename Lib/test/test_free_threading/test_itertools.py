8d91d4cbad019db5f6f1efb1cc71c6e2ff4f483f
Pieter Eendebak <pieter.eendebak@gmail.com>
2026-03-16 09:53:37 +0100

    [3.14] gh-123471: make concurrent iteration over itertools.accumulate thread-safe (#144486)
    (cherry picked from commit 3a248564470075cb8c7b8a75fe7ba61f7ea341b2)
    
    Co-authored-by: Pieter Eendebak <pieter.eendebak@gmail.com>

--- Lib/test/test_free_threading/test_itertools.py
+++ Lib/test/test_free_threading/test_itertools.py
@@ -1,11 +1,30 @@
 import unittest
 from itertools import (
+    accumulate,
     tee,
 )
 from test.support import threading_helper
 
 
 threading_helper.requires_working_threading(module=True)
+
+
+def work_iterator(it):
+    while True:
+        try:
+            next(it)
+        except StopIteration:
+            break
+
+
+class ItertoolsThreading(unittest.TestCase):
+
+    @threading_helper.reap_threads
+    def test_accumulate(self):
+        number_of_iterations = 10
+        for _ in range(number_of_iterations):
+            it = accumulate(tuple(range(40)))
+            threading_helper.run_concurrently(work_iterator, nthreads=10, args=[it])
 
 
 class TestTeeConcurrent(unittest.TestCase):
